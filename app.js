e03c9cc891c67585841a91c6323d218c7b461fcb
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2017-11-10 16:46:28 -0500

    Added some files into views

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 
 const app = express();
+let env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
-nunjucks.configure('views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(express.static('/', 'public'));
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
