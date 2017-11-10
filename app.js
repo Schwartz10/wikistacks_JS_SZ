2c596c3ad7fe42f0fb4afec87b58b9e5707f7701
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2017-11-10 17:25:28 -0500

    Currently on Getters

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
+const models = require('./models');
+const chalk =  require('chalk');
 
 const app = express();
 let env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 
+
+models.db.sync()
+  .then(result => console.log('page is synced'))
+  .then(() => app.listen(3000, () => console.log('killing it on port 3000')
+  ))
+  .catch(chalk.blue);
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(express.static('/', 'public'));
+app.use('/', express.static('public'));
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
-});
-
-app.listen(3000, () => {
-  console.log('killing it on port 3000');
 });
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+
+
+
+//BRAINSTORMING FOR GETTER FUNCTIONS
+router.get('/wiki/:urlTitle', function(req, res){
+  let urlTitle = req.params.urlTitle;
+
+
+})
